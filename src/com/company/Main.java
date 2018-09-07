9609d9c281ba439e6dfe5b865afedd3f8b4aa96d
VincentBott <vincentbott@gmail.com>
2018-09-07 16:03:15 +0200

    Laatste versie.  XML en SaxParser en MyContentHandler()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
+import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 import org.xml.sax.helpers.DefaultHandler;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -11,8 +13,8 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.*;
-
-
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main {
@@ -34,9 +36,26 @@ public class Main {
             spf.setSchema(schema);
             spf.setNamespaceAware(true);
             SAXParser saxParser = spf.newSAXParser();
-            saxParser.parse("boeken.xml", new MyContentHandler());
 
-            System.out.println("Validatie is gelukt");
+            //    saxParser.parse("boeken.xml", new MyContentHandler());
+
+            //    System.out.println("Validatie is gelukt");
+
+        //    List<String> titels = new ArrayList<>();
+        //    saxParser.parse("boeken.xml", new MyContentHandler(titels));
+        //    System.out.println("\nDe titels:\n");
+
+        //    titels.forEach(System.out::println);
+
+            List<Boek> boeken = new ArrayList<>();
+            saxParser.parse("boeken.xml", new MyContentHandler(boeken));
+            System.out.println("De boeken:");
+            boeken.forEach(System.out::println);
+        /*
+            for (int i = 0; i < titels.size(); i++) {
+                System.out.printf("\"%s\"\n", titels.get(i).toString());
+            }
+        */
 
         } catch (SAXException | IOException | ParserConfigurationException ex) {
             System.out.println(ex.getMessage());
@@ -45,7 +64,72 @@ public class Main {
     }
 }
 
+class Boek{
+    private String titel;
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    @Override
+    public String toString() {
+        return "Boek{" +
+                "titel='" + titel + '\'' +
+                '}';
+    }
+}
+
 class MyContentHandler extends DefaultHandler {
+    private StringBuilder tekstBuilder = new StringBuilder();
+    private List<Boek> boeken;
+    private Boek boek;
+
+    public MyContentHandler(List<Boek> boeken) {
+        this.boeken = boeken;
+    }
+
+    @Override
+    public void startDocument() throws SAXException {
+        //System.out.println("Begin van het document");
+    }
+
+    @Override
+    public void endDocument() throws SAXException {
+        //System.out.println("Einde van het document");
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        if (localName.equals("boek")){
+            boek=new Boek();
+        }
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        tekstBuilder.append(ch, start, length);
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if (localName.equals("titel")){
+            boek.setTitel(tekstBuilder.toString());
+
+        } else if (localName.equals("boek")){
+            boeken.add(boek);
+        }
+        tekstBuilder.setLength(0); // terug leegmaken
+    }
+}
+
+/*
+class MyContentHandler extends DefaultHandler {
+
+    private StringBuilder tekstBuilder = new StringBuilder();
+
 
     @Override
     public void startDocument() throws SAXException {
@@ -56,4 +140,44 @@ class MyContentHandler extends DefaultHandler {
     public void endDocument() throws SAXException {
         System.out.println("Einde van het document");
     }
-}
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        System.out.printf("Begin element <%s>\n", localName);
+    }
+
+    @Override
+    public void error(SAXParseException e) throws SAXParseException {
+        throw e;
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        tekstBuilder.append(ch, start, length);
+    }
+/*
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        System.out.printf("De tekst van %s is \"%s\"%n\n", localName, tekstBuilder.toString());
+        tekstBuilder.setLength(0); // terug leegmaken
+    }
+*/
+
+/*
+    private List<String> titels;
+
+    public MyContentHandler(List<String> titels) {
+        this.titels = titels;
+    }
+*/
+/*
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if (localName.equals("titel")) {
+            titels.add(tekstBuilder.toString());
+        }
+        tekstBuilder.setLength(0); // terug leegmaken
+    }
+    */
+
+
